27ffac49224bce4ded466f296c30fa0df141e7a1
Dan Buch <d.buch@modcloth.com>
2013-08-09 21:54:30 -0400

    Renaming fakeHandler to topHandler
    since it isn't fake and it's at the top of the pipeline.

---
+++ top_handler.go
@@ -0,0 +1,30 @@
+package hookworm
+
+import (
+	"log"
+)
+
+type topHandler struct {
+	next Handler
+}
+
+func newTopHandler() *topHandler {
+	return &topHandler{}
+}
+
+func (me *topHandler) HandleGithubPayload(payload *GithubPayload) error {
+	if me.next != nil {
+		return me.next.HandleGithubPayload(payload)
+	}
+
+	log.Printf("WARNING: no next handler? %+v", me)
+	return nil
+}
+
+func (me *topHandler) NextHandler() Handler {
+	return me.next
+}
+
+func (me *topHandler) SetNextHandler(nextHandler Handler) {
+	me.next = nextHandler
+}
