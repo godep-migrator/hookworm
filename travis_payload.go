a901eaa64e6dc577bc017164f48a3b0331465a4e
Dan Buch <d.buch@modcloth.com>
2013-08-14 14:16:03 -0400

    Filling in tests for Travis payload deserialization

--- travis_payload.go
+++ travis_payload.go
@@ -1,19 +1,28 @@
 package hookworm
 
+import (
+	"time"
+)
+
 // TravisPayload is the struct representation of a JSON payload from Travis
 type TravisPayload struct {
-	ID         int               `json:"id"`
-	Repository *TravisRepository `json:"repository"`
-	Number     string            `json:"number"`
-	Config     *TravisConfig     `json"config"`
-	Status     int               `json:"status"`
-	Result     int               `json:"result"`
+	ID            int               `json:"id"`
+	Repository    *TravisRepository `json:"repository"`
+	Number        string            `json:"number"`
+	Config        interface{}       `json:"config"`
+	Status        int               `json:"status"`
+	Result        int               `json:"result"`
+	StatusMessage string            `json:"status_message"`
+	ResultMessage string            `json:"result_message"`
+	StartedAt     *time.Time        `json:"started_at"`
+	FinishedAt    *time.Time        `json:"finished_at"`
+	Duration      int               `json:"duration"`
 }
 
 // TravisRepository is how a repository is represented in a TravisPayload
 type TravisRepository struct {
-}
-
-// TravisConfig is the JSON representation of the '.travis.yml' file
-type TravisConfig struct {
+	ID        int    `json:"id"`
+	Name      string `json:"name"`
+	OwnerName string `json:"owner_name"`
+	URL       string `json:"url"`
 }
