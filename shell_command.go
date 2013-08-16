dd5827f6c986d97a868eb567e2322bc5223e0112
Dan Buch <d.buch@modcloth.com>
2013-08-16 01:23:05 -0400

    Pre-building go handlers to speed things up

--- shell_command.go
+++ shell_command.go
@@ -1,8 +1,10 @@
 package hookworm
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
+	"path"
 	"strings"
 	"time"
 )
@@ -22,6 +24,10 @@ func newShellCommand(interpreter, filePath string, timeout int) shellCommand {
 }
 
 func (me *shellCommand) configure(config string) error {
+	if err := me.preBuild(); err != nil {
+		return err
+	}
+
 	return me.runCmd(config, "configure")
 }
 
@@ -33,25 +39,52 @@ func (me *shellCommand) handleTravisPayload(payload string) error {
 	return me.runCmd(payload, "handle", "travis")
 }
 
-func (me *shellCommand) runCmd(stdin string, argv ...string) error {
-	var cmd *exec.Cmd
+func (me *shellCommand) preBuild() error {
+	if me.interpreter != "go run" {
+		return nil
+	}
 
+	if err := me.swapGoRunWithBinary(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (me *shellCommand) swapGoRunWithBinary() error {
+	workingDir := os.Getenv("HOOKWORM_WORKING_DIR")
+	if len(workingDir) < 1 {
+		return fmt.Errorf("Missing HOOKWORM_WORKING_DIR!")
+	}
+
+	filePath := me.filePath
+	me.filePath = ""
+	me.interpreter = "go"
+
+	outfile := path.Join(workingDir, strings.Split(path.Base(filePath), ".")[0])
+	err := me.runCmd("", "build", "-o", outfile, filePath)
+	if err != nil {
+		return err
+	}
+
+	me.interpreter = outfile
+
+	return nil
+}
+
+func (me *shellCommand) runCmd(stdin string, argv ...string) error {
 	var (
-		interpreter string
+		cmd         *exec.Cmd
 		commandArgs []string
 	)
 
-	if me.interpreter == "go run" {
-		interpreter = "go"
-		commandArgs = append(commandArgs, "run", me.filePath)
-	} else {
-		interpreter = me.interpreter
+	if me.filePath != "" {
 		commandArgs = append(commandArgs, me.filePath)
 	}
 
 	commandArgs = append(commandArgs, argv...)
 
-	cmd = exec.Command(interpreter, commandArgs...)
+	cmd = exec.Command(me.interpreter, commandArgs...)
 	cmd.Stdin = strings.NewReader(stdin)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
