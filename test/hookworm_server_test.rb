4b3706de6a4d84d756d392dffbdacb2ec3a444b2
Dan Buch <d.buch@modcloth.com>
2013-09-05 10:54:21 -0400

    Touching up the test page, index page, favicon, etc.

--- test/hookworm_server_test.rb
+++ test/hookworm_server_test.rb
@@ -10,6 +10,26 @@ Mtbb::SERVERS.each do |name, server|
       post_github_payload(server.port, :valid).first.code.must_equal '204'
     end
   end
+
+  describe "#{name} ancillary pages" do
+    include HookwormJunkDrawer
+
+    it 'has a test page' do
+      get_request(port: server.port, path: '/debug/test').code.must_equal '200'
+    end
+
+    it 'has a blank page' do
+      get_request(port: server.port, path: '/blank').code.must_equal '204'
+    end
+
+    it 'has an index page' do
+      get_request(port: server.port, path: '/').code.must_equal '200'
+    end
+
+    it 'has a favicon' do
+      get_request(port: server.port, path: '/favicon.ico').code.must_equal '200'
+    end
+  end
 end
 
 describe 'when receiving a payload for a watched branch' do
